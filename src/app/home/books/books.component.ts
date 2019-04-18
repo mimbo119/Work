2a01e621614f63f36ae171c13f937bf8de284f50
Meem <mimbo119@gmail.com>
2019-04-18 20:18:28 +0600

    third

--- src/app/home/books/books.component.ts
+++ src/app/home/books/books.component.ts
@@ -10,29 +10,27 @@ export class BooksComponent implements OnInit {
 
   constructor() { }
   
-
+  bookArray = [];
+  
   array = [
     { id: "1", name: 'X' },
+    { id: "2", name: 'YY' },
     { id: "2", name: 'Y' },
     { id: "3", name: 'Z' }
   ];
 
-  bookItem(){
+  bookItem(file){
     for (var i=0; i<=this.array.length; i++){
-      if(this.array[i].id == this.fileItem){
-        console.log("ok");
-        return i;
-        
+      if(this.array[i].id == file){
+        this.bookArray.push(this.array[i]);
       }
     }
   }
 
 
   ngOnInit() {
-    // let bookArray = null;
-    console.log(this.fileItem);
-    // bookArray = this.array[this.bookItem()];
-    this.bookItem();
+    this.bookItem(this.fileItem);
+    console.log(this.bookArray)
   }
 
 }
